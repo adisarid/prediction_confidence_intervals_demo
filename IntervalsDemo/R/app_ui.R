68fb0e9a0cbac66849cdbf745dbaa7a380cd7de4
adisarid <adi@sarid-ins.co.il>
2019-12-13 01:27:23 +0200

    fixed call to external resources which failed on shinyapps.io

--- IntervalsDemo/R/app_ui.R
+++ IntervalsDemo/R/app_ui.R
@@ -2,7 +2,7 @@
 app_ui <- function() {
   tagList(
     # Leave this function for adding external resources
-    golem_add_external_resources(),
+    # golem_add_external_resources(),
     # List the first level UI elements here 
     fluidPage(theme = shinythemes::shinytheme("united"), title = "Prediction and Confidence Intervals",
               fluidRow(mod_create_intervals_ui("create_intervals_ui_1"))
@@ -10,19 +10,19 @@ app_ui <- function() {
   )
 }
 
-#' @import shiny
-golem_add_external_resources <- function(){
-  
-  addResourcePath(
-    'www', system.file('app/www', package = 'IntervalsDemo')
-  )
- 
-  tags$head(
-    golem::activate_js(),
-    golem::favicon()
-    # Add here all the external resources
-    # If you have a custom.css in the inst/app/www
-    # Or for example, you can add shinyalert::useShinyalert() here
-    #tags$link(rel="stylesheet", type="text/css", href="www/custom.css")
-  )
-}
+#' #' @import shiny
+#' golem_add_external_resources <- function(){
+#' 
+#'   # addResourcePath(
+#'   #   'www', system.file('app/www', package = 'IntervalsDemo')
+#'   # )
+#' 
+#'   tags$head(
+#'     golem::activate_js(),
+#'     golem::favicon()
+#'     # Add here all the external resources
+#'     # If you have a custom.css in the inst/app/www
+#'     # Or for example, you can add shinyalert::useShinyalert() here
+#'     #tags$link(rel="stylesheet", type="text/css", href="www/custom.css")
+#'   )
+#' }
